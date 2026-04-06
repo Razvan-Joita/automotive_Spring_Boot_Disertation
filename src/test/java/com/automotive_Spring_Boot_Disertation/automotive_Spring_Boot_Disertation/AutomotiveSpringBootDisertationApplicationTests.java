77ec0d03a83cfddb15ec8a374424a0f5bb75e4b9
Razvan Joita <joitarazvan@yahoo.com>
2026-04-06 23:45:08 +0300

    Simplify test to avoid Spring context loading

--- src/test/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/AutomotiveSpringBootDisertationApplicationTests.java
+++ src/test/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/AutomotiveSpringBootDisertationApplicationTests.java
@@ -1,11 +1,7 @@
 package com.automotive_Spring_Boot_Disertation.automotive_Spring_Boot_Disertation;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class AutomotiveSpringBootDisertationApplicationTests {
 
 	@Test
