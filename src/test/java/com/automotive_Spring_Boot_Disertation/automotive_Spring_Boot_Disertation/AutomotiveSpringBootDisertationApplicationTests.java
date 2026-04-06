1e3a35bc103659774703c0d6a7f222cab47d3a03
Razvan Joita <joitarazvan@yahoo.com>
2026-04-06 23:38:12 +0300

    Solve issue regarding test

--- src/test/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/AutomotiveSpringBootDisertationApplicationTests.java
+++ src/test/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/AutomotiveSpringBootDisertationApplicationTests.java
@@ -2,12 +2,13 @@ package com.automotive_Spring_Boot_Disertation.automotive_Spring_Boot_Disertatio
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AutomotiveSpringBootDisertationApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
