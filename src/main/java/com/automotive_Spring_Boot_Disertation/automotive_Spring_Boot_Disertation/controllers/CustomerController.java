7aa23a8e0c06dd17e8c8e9691a2b7dab7ae611f4
Razvan Joita <joitarazvan@yahoo.com>
2026-04-30 17:12:39 +0300

    add versioning for the endpoint

--- src/main/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/controllers/CustomerController.java
+++ src/main/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/controllers/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1/employees")
 @RequiredArgsConstructor
 @Tag(name = "Customer", description = "CRUD operations for customers")
 public class CustomerController {
