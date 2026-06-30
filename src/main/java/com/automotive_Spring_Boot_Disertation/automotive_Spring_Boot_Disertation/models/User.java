777abab3c3762f29a8ce86d4880a4365cf0c3047
Razvan Joita <joitarazvan@yahoo.com>
2026-06-30 23:26:23 +0300

    add User Entity to fix the Hibernate ORM Problem

--- src/main/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/models/User.java
+++ src/main/java/com/automotive_Spring_Boot_Disertation/automotive_Spring_Boot_Disertation/models/User.java
@@ -1,14 +1,15 @@
 package com.automotive_Spring_Boot_Disertation.automotive_Spring_Boot_Disertation.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User extends BaseEntity {
-
     private String username;
     private String password;
     private String role;
